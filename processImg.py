835b70f85d110639bc676bf21d312e7fd1aaaacd
boonhaii <boonzxc@gmail.com>
2021-01-09 01:18:50 +0800

    Uploaded script with Img Process Func

---
+++ processImg.py
@@ -0,0 +1,89 @@
+import cv2
+import numpy as np
+import PIL
+from PIL import Image
+
+
+# == Parameters =======================================================================
+def removeBG(file):
+    BLUR = 21
+    CANNY_THRESH_1 = 10
+    CANNY_THRESH_2 = 200
+    MASK_DILATE_ITER = 10
+    MASK_ERODE_ITER = 10
+    MASK_COLOR = (0.0, 0.0, 1.0)  # In BGR format
+    # == Processing =======================================================================
+
+    # -- Read image -----------------------------------------------------------------------
+
+    img = cv2.imread(file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # -- Edge detection -------------------------------------------------------------------
+    edges = cv2.Canny(gray, CANNY_THRESH_1, CANNY_THRESH_2)
+    edges = cv2.dilate(edges, None)
+    edges = cv2.erode(edges, None)
+
+    # -- Find contours in edges, sort by area ---------------------------------------------
+    contour_info = []
+    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    # Previously, for a previous version of cv2, this line was:
+    #  contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    # Thanks to notes from commenters, I've updated the code but left this note
+    for c in contours:
+        contour_info.append((
+            c,
+            cv2.isContourConvex(c),
+            cv2.contourArea(c),
+        ))
+    contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
+    max_contour = contour_info[0]
+
+    # -- Create empty mask, draw filled polygon on it corresponding to largest contour ----
+    # Mask is black, polygon is white
+    mask = np.zeros(edges.shape)
+    cv2.fillConvexPoly(mask, max_contour[0], (255))
+
+    # -- Smooth mask, then blur it --------------------------------------------------------
+    mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
+    mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
+    mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
+    mask_stack = np.dstack([mask] * 3)  # Create 3-channel alpha mask
+
+    # -- Blend masked img into MASK_COLOR background --------------------------------------
+    mask_stack = mask_stack.astype('float32') / 255.0  # Use float matrices,
+    img = img.astype('float32') / 255.0  # for easy blending
+
+    masked = (mask_stack * img) + ((1 - mask_stack) * MASK_COLOR)  # Blend
+    masked = (masked * 255).astype('uint8')  # Convert back to 8-bit
+
+    # split image into channels
+    c_red, c_green, c_blue = cv2.split(img)
+
+    # merge with mask got on one of a previous steps
+    img_a = cv2.merge((c_red, c_green, c_blue, mask.astype('float32') / 255.0))
+
+    # save to disk
+    cv2.imwrite('out.png', img_a * 255)
+
+    # Get path of output file
+    path = file.split('/')
+    path.pop(-1)
+    path = '/' + '/'.join(path) + '/' + 'out.png'
+
+    return path
+
+
+def resize(file):
+    img = PIL.Image.open(file)
+    img = img.resize((512, 512))
+    img.save('r_img.png')
+    return img
+
+
+def processImg(file):
+    path = removeBG(file)
+    stickerImg = resize(path)
+    return stickerImg
+
+
